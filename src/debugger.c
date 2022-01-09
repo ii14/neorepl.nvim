a4b3e5375cc76da4d54536e59cf659ea2914cb43
ii14 <ii14@users.noreply.github.com>
2022-01-09 02:26:06 +0100

    Refactor debugger module

--- src/debugger.c
+++ src/debugger.c
@@ -1,16 +1,23 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-#ifndef NREPL_NO_YIELD_CHECK
-# include <luajit.h> // luajit required
-# if LUAJIT_VERSION_NUM < 20100
-#  include "lj_frame.h" // run make luajit-2.0
-# endif
+#include <luajit.h> // luajit required
+#if LUAJIT_VERSION_NUM < 20100
+# include "lj_frame.h" // run make luajit-2.0
 #endif
+
 
 #ifndef LUA_OK
 #define LUA_OK 0
+#endif
+
+#if LUAJIT_VERSION_NUM < 20100
+# define CAN_YIELD(L) (cframe_canyield((L)->cframe))
+#else
+# define CAN_YIELD(L) (lua_isyieldable(L))
 #endif
 
 #define NREPL_CURRENT "nrepl.current"
@@ -20,31 +27,41 @@
 #define MODE_NEXT 1
 #define MODE_FINISH 2
 
-#ifndef NREPL_NO_YIELD_CHECK
-# if LUAJIT_VERSION_NUM < 20100
-#  define CAN_YIELD(L) (cframe_canyield((L)->cframe))
-# else
-#  define CAN_YIELD(L) (lua_isyieldable(L))
-# endif
-#else
-# define CAN_YIELD(L) (1)
-#endif
 
 typedef struct {
-  int line;
+  int32_t id;
+  int32_t line;
   char *file;
 } debug_breakpoint;
 
 typedef struct {
+  // references
   int thread;
   int func;
+  // state
   int currentline;
   int skipline;
   int skiplevel;
-  int continuing;
-  size_t bplen;
+  bool continuing;
+  // breakpoints
+  int32_t bpid;
+  uint32_t bplen;
   debug_breakpoint *bps;
 } debug_userdata;
+
+#define DEBUG_USERDATA_INIT \
+  (debug_userdata){ \
+    .thread = LUA_REFNIL, \
+    .func = LUA_REFNIL, \
+    .currentline = -1, \
+    .skipline = -1, \
+    .skiplevel = -1, \
+    .continuing = false, \
+    .bpid = 0, \
+    .bplen = 0, \
+    .bps = NULL, \
+  }
+
 
 static int getlevel(lua_State *L)
 {
@@ -55,21 +72,22 @@ static int getlevel(lua_State *L)
   return level;
 }
 
-static int canresume(lua_State *L)
+static bool canresume(lua_State *L)
 {
   int status = lua_status(L);
   if (status == LUA_YIELD)
-    return 1; // suspended
+    return true; // suspended
   if (status == LUA_OK) {
     lua_Debug ar;
     if (lua_getstack(L, 0, &ar) > 0)
-      return 1; // normal. can it resume?
+      return true; // normal. can it resume?
     if (lua_gettop(L) == 0)
-      return 0; // dead
-    return 1; // suspended
+      return false; // dead
+    return true; // suspended
   }
-  return 0;
+  return false; // dead
 }
+
 
 static void hook(lua_State *L, lua_Debug *ar)
 {
@@ -94,26 +112,26 @@ static void hook(lua_State *L, lua_Debug *ar)
   if (data->continuing) { // TODO: move to a separate hook function
     if (data->bplen == 0 || !CAN_YIELD(L))
       return;
-    int gotinfo = 0;
+    bool gotinfo = false;
     for (size_t i = 0; i < data->bplen; ++i) {
       debug_breakpoint *bp = &data->bps[i];
       // check line first, since it's cheap
-      if (ar->currentline != bp->line)
+      if (ar->currentline < 1 || ar->currentline != bp->line)
         return;
 
       // get source info if necessary, once
       if (!gotinfo) {
         if (!lua_getinfo(L, "S", ar))
           return;
-        gotinfo = 1;
+        gotinfo = true;
       }
 
       // skip if we don't know the file
       if (*(ar->source) != '@')
         continue;
 
-      // yield if everything checks out
-      if (strcmp(bp->file, ar->short_src) == 0) {
+      // compare source files after the '@'
+      if (strcmp(bp->file, ar->source + 1) == 0) {
         if (ar->currentline != data->skipline) {
           data->skipline = ar->currentline;
           lua_yield(L, 0);
@@ -166,11 +184,14 @@ static int debugger_hook(lua_State *L, int mode)
     data->skiplevel = -1;
   }
 
-  data->continuing = 0;
+  data->continuing = false;
   lua_sethook(thread, hook, LUA_MASKLINE, 0);
   int status = lua_resume(thread, 0);
   lua_sethook(thread, NULL, 0, 0); // TODO: restore previous hook
   data->skiplevel = -1;
+
+  lua_pushnil(L);
+  lua_setfield(L, LUA_REGISTRYINDEX, NREPL_CURRENT);
 
   if (status == LUA_OK) { // coroutine returned
     lua_pushboolean(L, 1);
@@ -216,7 +237,7 @@ static int debugger_continue(lua_State *L)
   lua_pushvalue(L, -2);
   lua_setfield(L, LUA_REGISTRYINDEX, NREPL_CURRENT);
 
-  data->continuing = 1;
+  data->continuing = true;
   int status;
   if (data->bplen > 0) { // set hook if there are any breakpoints set
     lua_sethook(thread, hook, LUA_MASKLINE, 0);
@@ -226,6 +247,9 @@ static int debugger_continue(lua_State *L)
   } else {
     status = lua_resume(thread, 0);
   }
+
+  lua_pushnil(L);
+  lua_setfield(L, LUA_REGISTRYINDEX, NREPL_CURRENT);
 
   if (status == LUA_OK) { // coroutine returned
     lua_pushboolean(L, 1);
@@ -243,14 +267,14 @@ static int debugger_continue(lua_State *L)
   return 0; // unreachable
 }
 
-static int debugger_breakpoint(lua_State *L)
+static int debugger_breakpoint_add(lua_State *L)
 {
   debug_userdata *data = (debug_userdata *)luaL_checkudata(L, 1, NREPL_THREAD);
   const char *file = luaL_checkstring(L, 2);
   int line = luaL_checkinteger(L, 3);
   if (file == NULL || *file == '\0')
     luaL_error(L, "no file name");
-  if (line <= 0)
+  if (line < 1)
     luaL_error(L, "line smaller than 1");
 
   size_t slen = strlen(file);
@@ -266,10 +290,15 @@ static int debugger_breakpoint(lua_State *L)
     luaL_error(L, "realloc");
   }
 
-  nbps[nlen - 1] = (debug_breakpoint){ .line = line, .file = sfile };
+  uint32_t id = ++data->bpid;
+  nbps[nlen - 1] = (debug_breakpoint){
+    .id = id,
+    .line = line,
+    .file = sfile,
+  };
   data->bps = nbps;
   data->bplen = nlen;
-  lua_pushinteger(L, nlen);
+  lua_pushinteger(L, id);
   return 1;
 }
 
@@ -279,19 +308,14 @@ static int debugger_create(lua_State *L)
 
   lua_State *thread = lua_newthread(L);
   lua_pushvalue(L, 1);
-  lua_xmove(L, thread, 1); // move function to new thread
+  lua_xmove(L, thread, 1); // move function to the new thread
 
   debug_userdata *data = (debug_userdata *)lua_newuserdata(L, sizeof(debug_userdata));
+  *data = DEBUG_USERDATA_INIT;
   lua_pushvalue(L, -2); // duplicate thread and save the reference
   data->thread = luaL_ref(L, LUA_REGISTRYINDEX);
   lua_pushvalue(L, 1); // duplicate function and save the reference
   data->func = luaL_ref(L, LUA_REGISTRYINDEX);
-  data->currentline = -1;
-  data->skipline = -1;
-  data->skiplevel = -1;
-  data->continuing = 0;
-  data->bplen = 0;
-  data->bps = NULL;
 
   luaL_getmetatable(L, NREPL_THREAD);
   lua_setmetatable(L, -2);
@@ -306,56 +330,46 @@ static int debugger_index(lua_State *L)
   const char *index = lua_tostring(L, 2);
   if (strcmp(index, "thread") == 0) {
     lua_getref(L, data->thread);
-    return 1;
   } else if (strcmp(index, "func") == 0) {
     lua_getref(L, data->func);
-    return 1;
   } else if (strcmp(index, "currentline") == 0) {
     lua_pushinteger(L, data->currentline);
-    return 1;
   } else if (strcmp(index, "status") == 0) {
     lua_getref(L, data->thread);
     lua_State *thread = lua_tothread(L, -1);
     if (L == thread) {
       lua_pushliteral(L, "running");
-      return 1;
-    }
-    int status = lua_status(thread);
-    if (status == LUA_YIELD) {
-      lua_pushliteral(L, "suspended");
-      return 1;
-    } else if (status == LUA_OK) {
-      lua_Debug ar;
-      if (lua_getstack(thread, 0, &ar) > 0) {
-        lua_pushliteral(L, "normal");
-      } else if (lua_gettop(thread) == 0) {
-        lua_pushliteral(L, "dead");
-      } else {
-        lua_pushliteral(L, "suspended");
-      }
-      return 1;
     } else {
-      lua_pushliteral(L, "dead");
-      return 1;
+      int status = lua_status(thread);
+      if (status == LUA_YIELD) {
+        lua_pushliteral(L, "suspended");
+      } else if (status == LUA_OK) {
+        lua_Debug ar;
+        if (lua_getstack(thread, 0, &ar) > 0) {
+          lua_pushliteral(L, "normal");
+        } else if (lua_gettop(thread) == 0) {
+          lua_pushliteral(L, "dead");
+        } else {
+          lua_pushliteral(L, "suspended");
+        }
+      } else {
+        lua_pushliteral(L, "dead");
+      }
     }
   } else if (strcmp(index, "next") == 0) {
     lua_pushcfunction(L, debugger_next);
-    return 1;
   } else if (strcmp(index, "step") == 0) {
     lua_pushcfunction(L, debugger_step);
-    return 1;
   } else if (strcmp(index, "finish") == 0) {
     lua_pushcfunction(L, debugger_finish);
-    return 1;
   } else if (strcmp(index, "continue") == 0) {
     lua_pushcfunction(L, debugger_continue);
-    return 1;
   } else if (strcmp(index, "breakpoint") == 0) {
-    lua_pushcfunction(L, debugger_breakpoint);
-    return 1;
+    lua_pushcfunction(L, debugger_breakpoint_add);
+  } else {
+    return 0;
   }
-
-  return 0;
+  return 1;
 }
 
 static int debugger_gc(lua_State *L)
