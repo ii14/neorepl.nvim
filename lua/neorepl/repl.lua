d35f504941d9811e2f085aca205477f0298672c7
ii14 <ii14@users.noreply.github.com>
2022-08-05 22:18:11 +0200

    feat: set keywordprg to :help by default

--- lua/neorepl/repl.lua
+++ lua/neorepl/repl.lua
@@ -68,6 +68,7 @@ function Repl.new(config)
   vim.opt_local.relativenumber = false
   vim.opt_local.signcolumn = 'yes'
   vim.opt_local.backspace = 'indent,start'
+  vim.opt_local.keywordprg = ':help'
   api.nvim_buf_set_name(bufnr, 'neorepl://neorepl('..bufnr..')')
   -- set filetype after mappings and settings to allow overriding in ftplugin
   api.nvim_buf_set_option(bufnr, 'filetype', 'neorepl')
